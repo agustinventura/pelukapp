3a0fbf8ba1940a553d916a393ee113e471145a32
Jose Luis Montes Jimenez <jlmontesj@gmail.com>
2016-05-12 12:37:49 +0200

    rename

--- serverapp/src/main/java/com/spanishcoders/PelukappApplication.java
+++ serverapp/src/main/java/com/spanishcoders/PelukappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PelukaapApplication {
+public class PelukappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PelukaapApplication.class, args);
+		SpringApplication.run(PelukappApplication.class, args);
 	}
 
 }
