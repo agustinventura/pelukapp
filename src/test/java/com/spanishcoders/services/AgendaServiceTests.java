3842d524a6f2f65dabe42b3a9d79cfdca9767edf
Agustín <agustinventura@gmail.com>
2016-09-08 18:16:30 +0200

    Adds tests for AgendaService and HairdresserService.

--- src/test/java/com/spanishcoders/services/AgendaServiceTests.java
+++ src/test/java/com/spanishcoders/services/AgendaServiceTests.java
@@ -2,10 +2,7 @@ package com.spanishcoders.services;
 
 import com.google.common.collect.Sets;
 import com.spanishcoders.PelukaapUnitTest;
-import com.spanishcoders.model.Agenda;
-import com.spanishcoders.model.Block;
-import com.spanishcoders.model.Stretch;
-import com.spanishcoders.model.Timetable;
+import com.spanishcoders.model.*;
 import com.spanishcoders.repositories.AgendaRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
+import java.util.SortedMap;
 
 import static com.spanishcoders.TestDataFactory.*;
 import static org.hamcrest.Matchers.is;
@@ -119,12 +117,72 @@ public class AgendaServiceTests extends PelukaapUnitTest {
     }
 
     @Test
-    public void getBlocksForDay() {
-
+    public void getAvailableBlocksWithoutAgenda() {
+        Set<Block> blocks = agendaService.getAvailableBlocks(null, null, null);
+        assertThat(blocks.size(), is(0));
     }
 
     @Test
-    public void getBlocksForNonWorkingDay() {
+    public void getAvailableBlocksWithoutWorks() {
+        Set<Block> blocks = agendaService.getAvailableBlocks(mock(Agenda.class), null, null);
+        assertThat(blocks.size(), is(0));
+    }
 
+    @Test
+    public void getAvailableBlocksWithEmptyWorks() {
+        Set<Block> blocks = agendaService.getAvailableBlocks(mockAgenda(), Sets.newHashSet(), null);
+        assertThat(blocks.size(), is(0));
+    }
+
+    @Test
+    public void getAvailableBlocksWithoutDate() {
+        Set<Block> blocks = agendaService.getAvailableBlocks(mockAgenda(), Sets.newHashSet(mock(Work.class)), null);
+        assertThat(blocks.size(), is(0));
+    }
+
+    @Test
+    public void getAvailableBlocksForNonWorkingDay() {
+        Agenda agenda = mock(Agenda.class);
+        given(agenda.hasWorkingDay(any(LocalDate.class))).willReturn(false);
+        given(agenda.isNonWorkingDay(any(LocalDate.class))).willReturn(true);
+        Set<Block> blocks = agendaService.getAvailableBlocks(agenda, Sets.newHashSet(mock(Work.class)), LocalDate.now());
+        assertThat(blocks.size(), is(0));
+    }
+
+    @Test
+    public void getAvailableBlocksExistingDay() {
+        Agenda agenda = mock(Agenda.class);
+        given(agenda.hasWorkingDay(any(LocalDate.class))).willReturn(true);
+        WorkingDay workingDay = mock(WorkingDay.class);
+        SortedMap<LocalDate, WorkingDay> workingDays = mock(SortedMap.class);
+        given(agenda.getWorkingDays()).willReturn(workingDays);
+        given(workingDays.get(any(LocalDate.class))).willReturn(workingDay);
+        Set<Block> blocks = Sets.newHashSet(mock(Block.class));
+        given(workingDay.getAvailableBlocks(any(Set.class))).willReturn(blocks);
+        Set<Block> returnedBlocks = agendaService.getAvailableBlocks(agenda, Sets.newHashSet(mock(Work.class)), LocalDate.now());
+        assertThat(returnedBlocks, is(blocks));
+    }
+
+    @Test
+    public void getAvailableBlocksNonExistingDay() {
+        Agenda agenda = mock(Agenda.class);
+        given(agenda.hasWorkingDay(any(LocalDate.class))).willReturn(false);
+        given(agenda.isNonWorkingDay(any(LocalDate.class))).willReturn(false);
+        Stretch stretch = mock(Stretch.class);
+        given(stretch.getStart()).willReturn(LocalTime.now().minusHours(1));
+        given(stretch.getEnd()).willReturn(LocalTime.now().plusHours(1));
+        Set<Stretch> stretches = Sets.newHashSet(stretch);
+        Timetable timetable = mock(Timetable.class);
+        given(agenda.getCurrentTimetable()).willReturn(timetable);
+        given(timetable.getStretches()).willReturn(stretches);
+        given(agendaRepository.save(any(Agenda.class))).willReturn(agenda);
+        WorkingDay workingDay = mock(WorkingDay.class);
+        SortedMap<LocalDate, WorkingDay> workingDays = mock(SortedMap.class);
+        given(agenda.getWorkingDays()).willReturn(workingDays);
+        given(workingDays.get(any(LocalDate.class))).willReturn(workingDay);
+        Set<Block> blocks = Sets.newHashSet(mock(Block.class));
+        given(workingDay.getAvailableBlocks(any(Set.class))).willReturn(blocks);
+        Set<Block> returnedBlocks = agendaService.getAvailableBlocks(agenda, Sets.newHashSet(mock(Work.class)), LocalDate.now());
+        assertThat(returnedBlocks, is(blocks));
     }
 }
--- src/test/java/com/spanishcoders/services/HairdresserServiceTests.java
+++ src/test/java/com/spanishcoders/services/HairdresserServiceTests.java
@@ -2,15 +2,13 @@ package com.spanishcoders.services;
 
 import com.google.common.collect.Sets;
 import com.spanishcoders.PelukaapUnitTest;
-import com.spanishcoders.model.Agenda;
-import com.spanishcoders.model.Block;
-import com.spanishcoders.model.Hairdresser;
-import com.spanishcoders.model.UserStatus;
+import com.spanishcoders.model.*;
 import com.spanishcoders.repositories.HairdresserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Set;
 
@@ -72,7 +70,34 @@ public class HairdresserServiceTests extends PelukaapUnitTest {
     }
 
     @Test
-    public void getAvailableBlocksForDay() {
+    public void getAvailableBlocksForDayEmptyWorks() {
+        Map<Hairdresser, Set<Block>> availableBlocks = hairdresserService.getAvailableBlocksForDayByHairdresser(Sets.newHashSet(), LocalDate.now());
+        assertThat(availableBlocks.entrySet(), is(empty()));
+    }
 
+    @Test
+    public void getAvailableBlocksForDayNullWorks() {
+        Map<Hairdresser, Set<Block>> availableBlocks = hairdresserService.getAvailableBlocksForDayByHairdresser(null, LocalDate.now());
+        assertThat(availableBlocks.entrySet(), is(empty()));
+    }
+
+    @Test
+    public void getAvailableBlocksForDayNullDate() {
+        Set<Work> mockWorks = Sets.newHashSet(mock(Work.class));
+        Map<Hairdresser, Set<Block>> availableBlocks = hairdresserService.getAvailableBlocksForDayByHairdresser(mockWorks, null);
+        assertThat(availableBlocks.entrySet(), is(empty()));
+    }
+
+    @Test
+    public void getAvailableBlocksForDay() {
+        Hairdresser mockHairdresser = mock(Hairdresser.class);
+        given(hairdresserRepository.findByStatus(any(UserStatus.class))).willReturn(Sets.newHashSet(mockHairdresser));
+        Block mockBlock = mock(Block.class);
+        Set<Block> blocks = Sets.newHashSet(mockBlock);
+        given(agendaService.getAvailableBlocks(any(Agenda.class), any(Set.class), any(LocalDate.class))).willReturn(blocks);
+        Set<Work> mockWorks = Sets.newHashSet(mock(Work.class));
+        Map<Hairdresser, Set<Block>> returnedBlocks = hairdresserService.getAvailableBlocksForDayByHairdresser(mockWorks, LocalDate.now());
+        assertThat(returnedBlocks.size(), is(1));
+        assertThat(returnedBlocks.get(mockHairdresser).size(), is(blocks.size()));
     }
 }
