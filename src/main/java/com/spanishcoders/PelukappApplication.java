fda13aabd5e6ee8c35f6bf339f3792bd35e5bcd7
Agustín Ventura <agustinventura@gmail.com>
2017-05-12 11:06:05 +0200

    Updates libraries versions.

--- src/main/java/com/spanishcoders/PelukappApplication.java
+++ src/main/java/com/spanishcoders/PelukappApplication.java
@@ -2,12 +2,15 @@ package com.spanishcoders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+@EntityScan(basePackageClasses = { PelukappApplication.class, Jsr310JpaConverters.class })
 @SpringBootApplication
 public class PelukappApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PelukappApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PelukappApplication.class, args);
+	}
 
 }
