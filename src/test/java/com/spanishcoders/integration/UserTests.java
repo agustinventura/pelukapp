b103ee13446cf3659bc23ef7e0b1c229b6104d34
Agustín Ventura <agustinventura@gmail.com>
2017-06-02 14:17:28 +0200

    Deletes old test

--- src/test/java/com/spanishcoders/integration/UserTests.java
+++ src/test/java/com/spanishcoders/integration/UserTests.java
@@ -1,25 +1,16 @@
 package com.spanishcoders.integration;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
-
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.spanishcoders.appointment.AppointmentDTO;
 import com.spanishcoders.user.UserDTO;
 
 public class UserTests extends IntegrationTests {
-
-	public final static String NEXT_APPOINTMENTS_URL = "/user/appointments/next";
 
 	@Before
 	public void setUp() {
@@ -48,19 +39,5 @@ public class UserTests extends IntegrationTests {
 	public void adminLogin() {
 		final UserDTO admin = login(ADMIN_USERNAME, ADMIN_PASSWORD).getBody();
 		assertThat(admin.getUsername(), is(ADMIN_USERNAME));
-	}
-
-	@Test
-	public void getNextAppointments() {
-		final String auth = loginAsClient();
-		final AppointmentDTO appointment = integrationDataFactory.getAppointment(auth);
-		final HeadersTestRestTemplate<Set<AppointmentDTO>> appointmentClient = new HeadersTestRestTemplate<>(
-				testRestTemplate);
-		final ParameterizedTypeReference<Set<AppointmentDTO>> appointmentTypeRef = new ParameterizedTypeReference<Set<AppointmentDTO>>() {
-		};
-		final Set<AppointmentDTO> nextAppointments = appointmentClient.getWithAuthorizationHeader(NEXT_APPOINTMENTS_URL,
-				auth, appointmentTypeRef);
-		assertThat(nextAppointments, not(empty()));
-		assertThat(nextAppointments, hasItem(appointment));
 	}
 }
