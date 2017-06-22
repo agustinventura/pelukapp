a792b1d34f1a0f9b20f2fb0dd90196cbbeeb490e
Agustín Ventura <agustinventura@gmail.com>
2017-06-22 13:43:32 +0200

    Adds sunday as nonworkingday in development data.

--- src/main/java/com/spanishcoders/configuration/development/DevelopmentDataBaseConfiguration.java
+++ src/main/java/com/spanishcoders/configuration/development/DevelopmentDataBaseConfiguration.java
@@ -1,5 +1,6 @@
 package com.spanishcoders.configuration.development;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -110,10 +111,16 @@ public class DevelopmentDataBaseConfiguration {
 		final Timetable timetable = new Timetable(LocalDate.now().minusDays(365), LocalDate.now().plusDays(365),
 				morning, afternoon);
 		final Agenda agenda = new Agenda(admin, timetable);
-		agenda.addNonWorkingDay(LocalDate.of(2016, 01, 01));
-		agenda.addNonWorkingDay(LocalDate.of(2016, 01, 06));
-		agenda.addNonWorkingDay(LocalDate.of(2016, 02, 28));
-		agenda.addNonWorkingDay(LocalDate.of(2016, 8, 15));
+		agenda.addNonWorkingDay(LocalDate.of(LocalDate.now().getYear(), 01, 01));
+		agenda.addNonWorkingDay(LocalDate.of(LocalDate.now().getYear(), 01, 06));
+		agenda.addNonWorkingDay(LocalDate.of(LocalDate.now().getYear(), 02, 28));
+		agenda.addNonWorkingDay(LocalDate.of(LocalDate.now().getYear(), 8, 15));
+		for (int i = 0; i < 366; i++) {
+			final LocalDate day = LocalDate.now().plusDays(i);
+			if (day.getDayOfWeek() == DayOfWeek.SUNDAY) {
+				agenda.addNonWorkingDay(day);
+			}
+		}
 	}
 
 	private void createWorks(WorkRepository workRepository) {
